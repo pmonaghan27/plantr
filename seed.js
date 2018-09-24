acbcc9375b6f7c366856d3efda673ef7f379deb2
Patrick Monaghan <monaghanp3@gmail.com>
2018-09-24 18:12:35 -0400

    still no association table solution

--- seed.js
+++ seed.js
@@ -1,20 +1,19 @@
 const {db, Vegetable, Plot, Gardener} = require('./model');
-// const {} = db;
-// const {} = db;
-// const {} = db;
+const vegetablePlot = db.model('vegetable_plot');
 
 db.sync({force: true})
   .then(() => {
     console.log('db synced')
   })
   .then(() => {
-    return Vegetable.create({name: 'lettuce', color: 'green', planted_on: new Date()})
-  })
-  .then((veggie) => {
-    return Gardener.create({name: 'bob', age: 45, favoriteVegetableId: veggie.id})
-  })
-  .then((owner) => {
-    return Plot.create({size: 100, shaded: true, gardenerId: owner.id})
+    const vegetable = Vegetable.create({name: 'lettuce', color: 'green', planted_on: new Date()});
+    const gardener = vegetable.then((veggie) => {
+      return Gardener.create({name: 'bob', age: 45, favoriteVegetableId: veggie.id})
+    });
+    const plot = gardener.then((owner) => {
+      return Plot.create({size: 100, shaded: true, gardenerId: owner.id})
+    });
+    return plot;
   })
   .catch((error) => {
     console.log('db is not synced: ', error)
